9906f16a7f37efbb7c67520e82a0dd393b2b787f
Mark Opondo <markopondoi.t@gmail.com>
2026-03-22 17:57:49 +0300

    removed the function calls to pass the test

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 //Write your code here
+// initialise new object
+const attendee = {
+  attendeeId : "T001",
+  name : "Alice Smith",
+  event : "JavaScript Conference",
+  ticketType : "VIP",
+  ticketPrice : 150.00
+}
 
+// Function to log attendee name
+const logAttendeeName = (name) => {
+  console.log(attendee.name);
+}
+
+// Function to log the ticket price
+const logTicketPrice = (attendee) => {
+  console.log(attendee.ticketPrice);
+}
+
+// Function to update the ticket type
+const updateTicketType = (attendee, newType) => {
+  attendee.ticketType = newType
+  console.log(attendee)
+}
+
+// Function to update the ticket price
+const updateTicketPrice = (attendee, newPrice) => {
+  attendee.ticketPrice = newPrice
+  console.log(attendee)
+}
+
+// Function to remove the event property
+const removeEventProperty = (attendee) => {
+  delete attendee.event
+  console.log(attendee);
+}
+
+// Function to add new property in the object
+const addCheckedInProperty = (attendee) => {
+  attendee.checkedIn = true
+  console.log(attendee);
+}
+
+
+// logAttendeeName()
+// logTicketPrice(attendee)
+// updateTicketType(attendee, "VVIP")
+// updateTicketPrice(attendee, 200)
+// removeEventProperty(attendee)
+// addCheckedInProperty(attendee)
 
 
 //Needed for the tests to work. Don't modify
